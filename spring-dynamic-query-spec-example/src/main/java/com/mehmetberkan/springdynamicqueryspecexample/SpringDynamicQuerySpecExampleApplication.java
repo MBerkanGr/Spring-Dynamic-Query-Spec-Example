926d27023d7f058414d251a5faf30714e5f414c7
Mehmet Berkan <mehmetberkangorel@gmail.com>
2025-01-02 22:11:18 +0300

    Initial commit

---
+++ spring-dynamic-query-spec-example/src/main/java/com/mehmetberkan/springdynamicqueryspecexample/SpringDynamicQuerySpecExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.springdynamicqueryspecexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamicQuerySpecExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDynamicQuerySpecExampleApplication.class, args);
+	}
+
+}
---
+++ spring-dynamic-query-spec-example/src/test/java/com/mehmetberkan/springdynamicqueryspecexample/SpringDynamicQuerySpecExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.springdynamicqueryspecexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamicQuerySpecExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
